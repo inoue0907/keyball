e751ac3114c887de4932a6a91d70256e78708462
inoue0907 <yuto-inoue@g.ecc.u-tokyo.ac.jp>
2026-02-02 17:31:23 +0900

    Disable OLED support in VIA keymap configuration

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -73,14 +73,3 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 #endif
     return state;
 }
-
-#ifdef OLED_ENABLE
-
-#    include "lib/oledkit/oledkit.h"
-
-void oledkit_render_info_user(void) {
-    keyball_oled_render_keyinfo();
-    keyball_oled_render_ballinfo();
-    keyball_oled_render_layerinfo();
-}
-#endif
